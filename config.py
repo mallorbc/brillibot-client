057edbbdc30667fccd722efcb95df4694f1e1656
Blake Mallory <blakecmallory@gmail.com>
2022-10-18 15:27:54 -0400

    added actions config

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ class Config:
     energy_threshold:int = attrib(default=300)
     pause_threshold:int = attrib(default=0.8)
     dynamic_energy_threshold:bool = attrib(default=True)
+    actions_file:str = attrib(default="actions_flight.json")
+    # actions_file:str = attrib(default="actions_lights.json")
